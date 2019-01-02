e437fe15ccba2c01236eaf44993cb76d7059e5d9
ZGaurilcikas <zygimantas.gaurilcikas@gmail.com>
2019-01-02 08:12:14 +0200

    angular IP

--- footer.php
+++ footer.php
@@ -11,6 +11,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="assets/js/vendor/angular.min.js"></script>
 <script src="assets/js/app.js"></script>
+<script src="assets/js/custom.js"></script>
 <script src="assets/js/controllers/mainController.js"></script>
 <script src="assets/js/services/ips.js"></script>
 </body>
